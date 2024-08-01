9eb90aab3901a32def38cb811dd31ce4dbe92470
Raúl Caballero <14023048+rcaballerocode@users.noreply.github.com>
2024-07-31 19:40:54 -0500

    managed identities

--- azureproject/production.py
+++ azureproject/production.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING_NEW']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
@@ -37,7 +37,7 @@ DATABASES = {
         'NAME': conn_str_params['dbname'],
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password']
+        'PASSWORD': ''
     }
 }
 
@@ -52,4 +52,4 @@ CACHES = {
     }
 }
 
-#get_token()
+get_token()
